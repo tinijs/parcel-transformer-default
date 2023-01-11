9b2db5706ffab84264e967a4865125c42cc2c918
Lam Nhan <nhan.lh@raksul.com>
2023-01-11 11:09:07 +0700

    Move wbCode to app onCreate()

--- src/lib/pwa.ts
+++ src/lib/pwa.ts
@@ -1,13 +1,23 @@
 export function injectPWA(content: string) {
-  return (
-    content +
-    '\n' +
-    `
-import {Workbox} from 'workbox-window';
-if ('serviceWorker' in navigator) {
-  const wb = new Workbox('/sw.js');
-  wb.register();
-}
-`
-  );
+  const wbCode = `
+    if ('serviceWorker' in navigator) {
+      this.$workbox = new Workbox('/sw.js');
+      this.$workbox.register();
+    }
+  `;
+  // import workbox-window
+  content = "import {Workbox} from 'workbox-window';\n" + content;
+  // add code
+  const anchorMatching = content.match(/(onCreate\()([\s\S]*?)(\{)/);
+  if (anchorMatching) {
+    const anchorStr = anchorMatching[0];
+    content = content.replace(anchorStr, anchorStr + `\n${wbCode}`);
+  } else {
+    const anchorStr = 'export class AppRoot extends TiniComponent {';
+    content = content.replace(
+      anchorStr,
+      anchorStr + `\nonCreate() {${wbCode}}`
+    );
+  }
+  return content;
 }
